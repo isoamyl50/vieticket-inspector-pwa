27e35cb0f14f374312b3d57baf4932bc51e1b91c
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-07-04 09:09:37 +0700

    refactor: Improve accessibility of ThemeButton component

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -7,29 +7,29 @@ import BrightnessAutoIcon from '@mui/icons-material/BrightnessAuto';
 type ThemePreference = 'auto' | 'light' | 'dark';
 
 interface ThemeButtonProps {
-  className?: string;
-  userPref: ThemePreference;
-  cycleTheme: () => void; 
+    className?: string;
+    userPref: ThemePreference;
+    cycleTheme: () => void;
 }
 
 function ThemeButton({ userPref, cycleTheme, className }: ThemeButtonProps): JSX.Element {
-  const buttonVariant = {
-    auto: 'outline-secondary',
-    light: 'outline-dark',
-    dark: 'outline-light',
-  }[userPref] as string;
+    const buttonVariant = {
+        auto: 'outline-secondary',
+        light: 'outline-dark',
+        dark: 'outline-light',
+    }[userPref] as string;
 
-  const buttonIcon = {
-    auto: <BrightnessAutoIcon aria-label='Auto Light/Dark Mode' />,
-    light: <LightModeIcon aria-label='Light Mode' />,
-    dark: <DarkMOdeIcon aria-label='Dark Mode' />,
-  }[userPref];
+    const buttonIcon = {
+        auto: <BrightnessAutoIcon aria-label='Auto Light/Dark Mode' />,
+        light: <LightModeIcon aria-label='Light Mode' />,
+        dark: <DarkMOdeIcon aria-label='Dark Mode' />,
+    }[userPref];
 
-  return (
-    <Button aria-label='Toggle Light/Dark Mode' className={className} onClick={cycleTheme} variant={buttonVariant}>
-      {buttonIcon}
-    </Button>
-  );
+    return (
+        <Button aria-label='Toggle Light/Dark Mode' title='Toggle Light/Dark Mode' className={className} onClick={cycleTheme} variant={buttonVariant}>
+            {buttonIcon}
+        </Button>
+    );
 }
 
 export default ThemeButton;
